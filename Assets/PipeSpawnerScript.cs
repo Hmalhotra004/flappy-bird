ccab941e854816110ad56e373143e9fccbed1d52
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-03-21 23:07:34 +0530

    game

---
+++ Assets/PipeSpawnerScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PipeSpawnerScript : MonoBehaviour
+{
+
+    public GameObject pipe;
+    public float spwanRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spwanPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < spwanRate){
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spwanPipe();
+            timer = 0;
+        }
+    }
+
+    private void spwanPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Instantiate(pipe, new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint),0), transform.rotation);
+    }
+}
